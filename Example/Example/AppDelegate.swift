d259b374dcac6cfe7b6a5b65d7392f4c3e765208
QuintGao <1094887059@qq.com>
2022-03-24 14:05:58 +0800

    代码优化，修复bug

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        print(GKDevice.deviceModel())
         GKConfigure.awake()
         
         GKConfigure.setupCustom { (configure) in
