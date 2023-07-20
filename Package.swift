e3ecb60344534f0cdb7dcd041f9f3ef455894767
QuintGao <1094887059@qq.com>
2023-07-20 15:12:46 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,27 +6,16 @@ import PackageDescription
 let package = Package(
     name: "GKNavigationBarSwift",
     // 支持的平台和版本
-    platforms: [
-        .iOS(.v9)
-    ],
+    platforms: [.iOS(.v9)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "GKNavigationBarSwift",
             targets: ["GKNavigationBarSwift"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GKNavigationBarSwift",
-            path: "Sources",
-            resources: [.process("GKNavigationBarSwift.bundle")],
-            publicHeadersPath: "."
-        ),
+            path: "Sources"
+        )
     ]
 )
